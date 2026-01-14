90cd5d72e1bb2ed47b68d10ba141c118e6880cbb
ema <mccoy39082@163.com>
2026-01-14 10:59:12 +0800

    Add WithInRange extension for IQueryable
    Introduces the WithInRange extension method for IQueryable<T>, allowing range checking on an index and returning the original source. The method outputs a boolean indicating if the index is within the range of the source's count.

---
+++ src/InRangeExtension.Fluent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace System.InRange;
+
+public static partial class InRangeExtension
+{
+#if !NET35 && !NET40
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+    public static IQueryable<T> WithInRange<T>(this IQueryable<T> source, int index, out bool result)
+    {
+        result = InRange(index, source?.Count());
+        return source!;
+    }
+}
