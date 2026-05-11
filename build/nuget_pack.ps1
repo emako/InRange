0d3aa11e02e1c28b4f25e917e13db95e72911713
ema <mccoy39082@163.com>
2026-05-11 13:50:01 +0800

    Add package logo and nuget pack script

---
+++ build/nuget_pack.ps1
@@ -0,0 +1,20 @@
+﻿Set-Location $PSScriptRoot
+
+Write-Host @"
+███╗   ██╗██╗   ██╗ ██████╗ ███████╗████████╗
+████╗  ██║██║   ██║██╔════╝ ██╔════╝╚══██╔══╝
+██╔██╗ ██║██║   ██║██║  ███╗█████╗     ██║   
+██║╚██╗██║██║   ██║██║   ██║██╔══╝     ██║   
+██║ ╚████║╚██████╔╝╚██████╔╝███████╗   ██║   
+╚═╝  ╚═══╝ ╚═════╝  ╚═════╝ ╚══════╝   ╚═╝   
+"@
+
+Push-Location "..\src\"
+Write-Host "Processing System.InRange ..."
+dotnet restore
+dotnet build -c Release
+dotnet pack -c Release -o ../build/
+Pop-Location
+
+Write-Host "`nPress any key to exit..."
+[void][System.Console]::ReadKey($true)
